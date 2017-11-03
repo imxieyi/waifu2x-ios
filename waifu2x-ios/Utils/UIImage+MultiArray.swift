f5ba1276d50380689fe55a680a368eb9325dbc6b
Xie Yi <wez733@live.cn>
2017-11-03 18:16:01 +0800

    fix weird lines around the corner of output image

--- waifu2x-ios/Utils/UIImage+MultiArray.swift
+++ waifu2x-ios/Utils/UIImage+MultiArray.swift
@@ -83,6 +83,124 @@ extension UIImage {
                 arr[yy * exwidth + xx + exwidth * exheight * 2] = fb
             }
         }
+        // Top-left corner
+        pixel = 0
+        r = data[pixel]
+        g = data[pixel + 1]
+        b = data[pixel + 2]
+        fr = Float(r) / 255
+        fg = Float(g) / 255
+        fb = Float(b) / 255
+        for y in 0..<shrink_size {
+            for x in 0..<shrink_size {
+                arr[y * exwidth + x] = fr
+                arr[y * exwidth + x + exwidth * exheight] = fg
+                arr[y * exwidth + x + exwidth * exheight * 2] = fb
+            }
+        }
+        // Top-right corner
+        pixel = (width - 1) * 4
+        r = data[pixel]
+        g = data[pixel + 1]
+        b = data[pixel + 2]
+        fr = Float(r) / 255
+        fg = Float(g) / 255
+        fb = Float(b) / 255
+        for y in 0..<shrink_size {
+            for x in width+shrink_size..<width+2*shrink_size {
+                arr[y * exwidth + x] = fr
+                arr[y * exwidth + x + exwidth * exheight] = fg
+                arr[y * exwidth + x + exwidth * exheight * 2] = fb
+            }
+        }
+        // Bottom-left corner
+        pixel = (width * (height - 1)) * 4
+        r = data[pixel]
+        g = data[pixel + 1]
+        b = data[pixel + 2]
+        fr = Float(r) / 255
+        fg = Float(g) / 255
+        fb = Float(b) / 255
+        for y in height+shrink_size..<height+2*shrink_size {
+            for x in 0..<shrink_size {
+                arr[y * exwidth + x] = fr
+                arr[y * exwidth + x + exwidth * exheight] = fg
+                arr[y * exwidth + x + exwidth * exheight * 2] = fb
+            }
+        }
+        // Bottom-right corner
+        pixel = (width * (height - 1) + (width - 1)) * 4
+        r = data[pixel]
+        g = data[pixel + 1]
+        b = data[pixel + 2]
+        fr = Float(r) / 255
+        fg = Float(g) / 255
+        fb = Float(b) / 255
+        for y in height+shrink_size..<height+2*shrink_size {
+            for x in width+shrink_size..<width+2*shrink_size {
+                arr[y * exwidth + x] = fr
+                arr[y * exwidth + x + exwidth * exheight] = fg
+                arr[y * exwidth + x + exwidth * exheight * 2] = fb
+            }
+        }
+        // Top & bottom bar
+        for x in 0..<width {
+            pixel = x * 4
+            r = data[pixel]
+            g = data[pixel + 1]
+            b = data[pixel + 2]
+            fr = Float(r) / 255
+            fg = Float(g) / 255
+            fb = Float(b) / 255
+            xx = x + shrink_size
+            for y in 0..<shrink_size {
+                arr[y * exwidth + xx] = fr
+                arr[y * exwidth + xx + exwidth * exheight] = fg
+                arr[y * exwidth + xx + exwidth * exheight * 2] = fb
+            }
+            pixel = (width * (height - 1) + x) * 4
+            r = data[pixel]
+            g = data[pixel + 1]
+            b = data[pixel + 2]
+            fr = Float(r) / 255
+            fg = Float(g) / 255
+            fb = Float(b) / 255
+            xx = x + shrink_size
+            for y in height+shrink_size..<height+2*shrink_size {
+                arr[y * exwidth + xx] = fr
+                arr[y * exwidth + xx + exwidth * exheight] = fg
+                arr[y * exwidth + xx + exwidth * exheight * 2] = fb
+            }
+        }
+        // Left & right bar
+        for y in 0..<height {
+            pixel = (width * y) * 4
+            r = data[pixel]
+            g = data[pixel + 1]
+            b = data[pixel + 2]
+            fr = Float(r) / 255
+            fg = Float(g) / 255
+            fb = Float(b) / 255
+            yy = y + shrink_size
+            for x in 0..<shrink_size {
+                arr[yy * exwidth + x] = fr
+                arr[yy * exwidth + x + exwidth * exheight] = fg
+                arr[yy * exwidth + x + exwidth * exheight * 2] = fb
+            }
+            pixel = (width * y + (width - 1)) * 4
+            r = data[pixel]
+            g = data[pixel + 1]
+            b = data[pixel + 2]
+            fr = Float(r) / 255
+            fg = Float(g) / 255
+            fb = Float(b) / 255
+            yy = y + shrink_size
+            for x in width+shrink_size..<width+2*shrink_size {
+                arr[yy * exwidth + x] = fr
+                arr[yy * exwidth + x + exwidth * exheight] = fg
+                arr[yy * exwidth + x + exwidth * exheight * 2] = fb
+            }
+        }
         return arr
     }
     
