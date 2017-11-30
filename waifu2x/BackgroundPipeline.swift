4f3536aec76f521d7f15907d38cfd15e7001813a
Xie Yi <wez733@live.cn>
2017-11-30 11:40:02 +0800

    Revert "multi thread on model prediction"
    This reverts commit ba7a181c9be56904953e3b8dc445659925a5eb34.

--- waifu2x/BackgroundPipeline.swift
+++ waifu2x/BackgroundPipeline.swift
@@ -17,46 +17,32 @@ class BackgroundPipeline <T> {
     
     private let work_sem = DispatchSemaphore(value: 0)
     
-    private var wait_sems: [DispatchSemaphore] = []
-    
-    private let thread_sem = DispatchSemaphore(value: 1)
+    private let wait_sem = DispatchSemaphore(value: 1)
     
     private let queue = Queue<T>()
     
-    private var background: [DispatchQueue] = []
-    
-    private func getNextIndex() -> Int {
-        thread_sem.wait()
-        let i = index
-        index += 1
-        thread_sem.signal()
-        return i
-    }
+    private let background: DispatchQueue
     
     /// Constructor
     ///
     /// - Parameters:
     ///   - name: The unique name of the background thread
     ///   - count: How many objects will this pipeline receive?
-    init(_ name: String, count: Int, threads: Int = 1, task: @escaping (Int, T) -> Void) {
+    init(_ name: String, count: Int, task: @escaping (Int, T) -> Void) {
         self.count = count
-        for i in 0..<threads {
-            let queue = DispatchQueue(label: name + "\(i)")
-            background.append(queue)
-            let wait_sem = DispatchSemaphore(value: 1)
-            wait_sems.append(wait_sem)
-            queue.async {
-                var index = 0
-                while index < self.count {
-                    autoreleasepool {
-                        self.work_sem.wait()
-                        index = self.getNextIndex()
-                        task(index, self.queue.dequeue()!)
-                    }
+        background = DispatchQueue(label: name)
+        background.async {
+            var index = 0
+            while index < self.count {
+                autoreleasepool {
+                    self.work_sem.wait()
+                    task(index, self.queue.dequeue()!)
+                    index += 1
                 }
-                wait_sem.signal()
             }
+            self.wait_sem.signal()
         }
+        wait_sem.wait()
     }
     
     public func appendObject(_ obj: T) {
@@ -66,9 +52,8 @@ class BackgroundPipeline <T> {
     
     /// Wait for work to complete, must be called
     public func wait() {
-        for sem in wait_sems {
-            sem.wait()
-        }
+        wait_sem.wait()
+        wait_sem.signal()
     }
     
 }
